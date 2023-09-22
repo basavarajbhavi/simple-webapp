5f790a93ac7a47d2c42f8fef38ff0ff900759195
root <basavaraj.bhavi@pratian.com>
2023-09-22 15:18:23 +0530

    add

--- app.py
+++ app.py
@@ -1,42 +1,38 @@
 import os
 from flask import Flask
-from flaskext.mysql import MySQL      # For newer versions of flask-mysql 
-# from flask.ext.mysql import MySQL   # For older versions of flask-mysql
+from flask import render_template
+import socket
+import random
+import os
+
 app = Flask(__name__)
 
-mysql = MySQL()
+color_codes = {
+    "red": "#e74c3c",
+    "green": "#16a085",
+    "blue": "#2980b9",
+    "blue2": "#30336b",
+    "pink": "#be2edd",
+    "darkblue": "#130f40"
+}
 
-mysql_database_host = 'MYSQL_DATABASE_HOST' in os.environ and os.environ['MYSQL_DATABASE_HOST'] or  'localhost'
-
-# MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'db_user'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'Passw0rd'
-app.config['MYSQL_DATABASE_DB'] = 'employee_db'
-app.config['MYSQL_DATABASE_HOST'] = mysql_database_host
-mysql.init_app(app)
-
-conn = mysql.connect()
-
-cursor = conn.cursor()
+color = os.environ.get('APP_COLOR') or random.choice(["red","green","blue","blue2","darkblue","pink"])
 
 @app.route("/")
 def main():
-    return "Welcome!"
+    #return 'Hello'
+    print(color)
+    return render_template('hello.html', name=socket.gethostname(), color=color_codes[color])
 
-@app.route('/how are you')
-def hello():
-    return 'I am good, how about you?'
+@app.route('/color/<new_color>')
+def new_color(new_color):
+    return render_template('hello.html', name=socket.gethostname(), color=color_codes[new_color])
 
-@app.route('/read from database')
-def read():
-    cursor.execute("SELECT * FROM employees")
-    row = cursor.fetchone()
-    result = []
-    while row is not None:
-      result.append(row[0])
-      row = cursor.fetchone()
-
-    return ",".join(result)
+@app.route('/read_file')
+def read_file():
+    f = open("/data/testfile.txt")
+    contents = f.read()
+    return render_template('hello.html', name=socket.gethostname(), contents=contents, color=color_codes[color])
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port="8080")
